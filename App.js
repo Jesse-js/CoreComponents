ae6061a197c1811d90d2bc1692adf9b01236fc8d
Jesse-js <jesse-jjs@hotmail.com>
2024-02-16 12:55:19 -0300

    feat: add Image Core Component

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import { View, Text } from "react-native";
-
+import { View, Text, Image } from "react-native";
+const logoImg = require("./assets/adaptive-icon.png");
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
       <Text>
         <Text style={{ color: "white" }}> Hello</Text> World
       </Text>
+      <Image source={logoImg} style={{ width: 300, height: 300 }} />
+      <Image
+        source={{ uri: "https://picsum.photos/200" }}
+        style={{ width: 300, height: 300 }}
+      />
     </View>
   );
 }
