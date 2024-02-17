72dce59cbd5edca04244b2656ef40218b12c9ae8
Jesse-js <jesse-jjs@hotmail.com>
2024-02-17 14:53:29 -0300

    feat: add Pressable Core Component

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { View, Text, Image, ScrollView, Button } from "react-native";
+import { View, Text, Image, ScrollView, Button, Pressable } from "react-native";
 const logoImg = require("./assets/adaptive-icon.png");
 const state = false;
 export default function App() {
@@ -8,24 +8,28 @@ export default function App() {
         <Text>
           <Text style={{ color: "white" }}> Hello</Text> World
         </Text>
-        <Image source={logoImg} style={{ width: 300, height: 300 }} />
-        <Text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          ullamcorper accumsan dui a tempor. Morbi sed sem suscipit,
-          sollicitudin enim euismod, suscipit libero. Sed rhoncus vel turpis non
-          convallis. Ut euismod mi libero, sed tristique odio placerat in.
-          Praesent efficitur fringilla tellus, nec vulputate odio convallis at.
-          Etiam ante ipsum, mattis id elementum ac, tempus eu sapien. Proin
-          consectetur elementum enim ut vulputate. Proin condimentum elementum
-          urna sed pellentesque. Duis vestibulum massa vitae nisl gravida,
-          sagittis hendrerit eros vestibulum. Quisque gravida metus sed cursus
-          consequat. Donec feugiat vestibulum sapien, sit amet tincidunt est
-          condimentum ac. Praesent velit mauris, varius non. Duis vestibulum
-          massa vitae nisl gravida, sagittis hendrerit eros vestibulum. Quisque
-          gravida metus sed cursus consequat. Donec feugiat vestibulum sapien,
-          sit amet tincidunt est condimentum ac. Praesent velit mauris, varius
-          non.
-        </Text>
+        <Pressable onPress={() => console.log('Image pressed')}>
+          <Image source={logoImg} style={{ width: 300, height: 300 }} />
+        </Pressable>
+        <Pressable onPress={() => console.log('Text pressed')} >
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            ullamcorper accumsan dui a tempor. Morbi sed sem suscipit,
+            sollicitudin enim euismod, suscipit libero. Sed rhoncus vel turpis
+            non convallis. Ut euismod mi libero, sed tristique odio placerat in.
+            Praesent efficitur fringilla tellus, nec vulputate odio convallis
+            at. Etiam ante ipsum, mattis id elementum ac, tempus eu sapien.
+            Proin consectetur elementum enim ut vulputate. Proin condimentum
+            elementum urna sed pellentesque. Duis vestibulum massa vitae nisl
+            gravida, sagittis hendrerit eros vestibulum. Quisque gravida metus
+            sed cursus consequat. Donec feugiat vestibulum sapien, sit amet
+            tincidunt est condimentum ac. Praesent velit mauris, varius non.
+            Duis vestibulum massa vitae nisl gravida, sagittis hendrerit eros
+            vestibulum. Quisque gravida metus sed cursus consequat. Donec
+            feugiat vestibulum sapien, sit amet tincidunt est condimentum ac.
+            Praesent velit mauris, varius non.
+          </Text>
+        </Pressable>
         <Image
           source={{ uri: "https://picsum.photos/200" }}
           style={{ width: 300, height: 300 }}
